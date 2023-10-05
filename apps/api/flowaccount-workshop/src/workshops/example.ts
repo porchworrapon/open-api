51a52f4928d549ebfc4f3cac6a75b1dc74565801
Porsche <worrapon12345@gmail.com>
2023-10-05 22:37:35 +0700

    Complete

--- apps/api/flowaccount-workshop/src/workshops/example.ts
+++ apps/api/flowaccount-workshop/src/workshops/example.ts
@@ -2,7 +2,22 @@ import * as bankStatement from '../bankStatements.json'
 import * as expenseStatements from '../expenseStatements.json';
 
 export function example() {
-  console.log('Hello World')
+  const matchingElements = [];
+
+
+  bankStatement.forEach((element1) => {
+
+    expenseStatements.forEach((element2) => {
+      // Check if elements match exactly
+      if (element1.amount === element2.amount) {
+        // Add the matching element to the result array
+        matchingElements.push([element1, element2]);
+      }
+    });
+  });
+
+  console.log("Matching elements:", matchingElements);
+
 }
 
 
